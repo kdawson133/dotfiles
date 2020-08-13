1e9cbf1c947d8909fe09926e70393e000bf79e81
kdawson133 <kdawson133@gmail.com>
2020-08-13 11:19:13 +1000

    Updated

--- .config/i3/scripts/lock.sh
+++ .config/i3/scripts/lock.sh
@@ -1,5 +1,12 @@
-
-://github.com/kdawson133 @kirk133
+#!/bin/bash
+#  _       _                               __ ____ ____  
+# | |     | |                             /_ |___ \___ \ 
+# | | ____| | __ ___      _____  ___  _ __ | | __) |__) |
+# | |/ / _` |/ _` \ \ /\ / / __|/ _ \| '_ \| ||__ <|__ < 
+# |   < (_| | (_| |\ V  V /\__ \ (_) | | | | |___) |__) |
+# |_|\_\__,_|\__,_| \_/\_/ |___/\___/|_| |_|_|____/____/ 
+#
+# https://github.com/kdawson133 @kirk133
 #
 
 B='#00000000'  # blank
@@ -9,7 +16,7 @@ T='#5365a2ee'  # text
 W='#880000bb'  # wrong
 V='#285577bb'  # verifying
 
-/usr/bin/i3lock \
+$HOME/bin/i3lock \
 --insidevercolor=$C   \
 --ringvercolor=$V     \
 \
@@ -44,4 +51,3 @@ V='#285577bb'  # verifying
 # --timefont=comic-sans
 # --datefont=monofur
 # etc
-
